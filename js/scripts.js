5f34798840cbed22fdc5f049bd5f639f33038e2c
kwinkich <vankord21@gmail.com>
2023-05-11 17:37:14 +0300

    fix bug & add icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,158 @@
+//const one = document.getElementById('one');
+//const two = document.getElementById('two');
+//const three = document.getElementById('three');
+//const four = document.getElementById('four');
+//const five = document.getElementById('five');
+//const six = document.getElementById('six');
+//const seven = document.getElementById('seven');
+//const eight = document.getElementById('eight');
+//const nine = document.getElementById('nine');
+//const zero = document.getElementById('zero');
+//const minus = document.getElementById('minus');
+//const multiply = document.getElementById('multiply');
+//const plus = document.getElementById('plus');
+//const division = document.getElementById('division');
+//const delAll = document.getElementById('deleted-all');
+//const delLine = document.getElementById('deleted-line');
+//const result = document.getElementById('enter');
+//let line = document.getElementById('line-text');
+//let value = '';
+//let prevOperator = '';
+//let lastNum = '';
+
+//zero.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += zero.value;
+//	line.innerHTML = value;
+//})
+
+//one.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += one.value;
+//	line.innerHTML = value;
+//})
+//two.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += two.value;
+//	line.innerHTML = value;
+//})
+//three.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += three.value;
+//	line.innerHTML = value;
+//})
+
+//four.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += four.value;
+//	line.innerHTML = value;
+//})
+
+//five.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += five.value;
+//	line.innerHTML = value;
+//})
+
+//six.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += six.value;
+//	line.innerHTML = value;
+//})
+
+//seven.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += seven.value;
+//	line.innerHTML = value;
+//})
+
+//eight.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += eight.value;
+//	line.innerHTML = value;
+//})
+
+//nine.addEventListener('click', () => {
+//	value !== '' ? value += '' : console.log('');
+//	value += nine.value;
+//	line.innerHTML = value;
+//})
+
+
+//plus.addEventListener('click', () => {
+//	if (value === '') {
+//		value = '0';
+//	} else if (!/[+\-*\/]$/.test(value)) {
+//		value += '+';
+//	}
+//	prevOperator = '+';
+//	line.innerHTML = value.replace(/([+\-*\/])/g, ' $1 ');
+//});
+
+//minus.addEventListener('click', () => {
+//	if (value === '') {
+//		value = '0';
+//	} else if (!/[+\-*\/]$/.test(value)) {
+//		value += '-';
+//	}
+//	prevOperator = '-';
+//	line.innerHTML = value.replace(/([+\-*\/])/g, ' $1 ');
+//});
+
+//multiply.addEventListener('click', () => {
+//	if (value === '') {
+//		value = '0';
+//	} else if (!/[+\-*\/]$/.test(value)) {
+//		value += '*';
+//	}
+//	prevOperator = '*';
+//	line.innerHTML = value.replace(/([+\-*\/])/g, ' $1 ');
+//});
+
+//division.addEventListener('click', () => {
+//	if (value === '') {
+//		value = '0';
+//	} else if (!/[+\-*\/]$/.test(value)) {
+//		value += '/';
+//	}
+//	prevOperator = '/';
+//	line.innerHTML = value.replace(/([+\-*\/])/g, ' $1 ');
+//});
+
+//result.addEventListener('click', () => {
+//	if (value === '') {
+//		line.innerHTML = '0';
+//	} else {
+//		line.innerHTML = eval(value).toString();
+//		value = line.innerHTML;
+//		prevOperator = '';
+//	}
+//});
+
+//delAll.addEventListener('click', () => {
+//	value = '';
+//	prevOperator = '';
+//	line.innerHTML = '0';
+//});
+
+////delAll.addEventListener('click', () => {
+////	line.innerHTML = '0';
+////	value = '';
+////});
+
+////result.addEventListener('click', () => {
+////	const result = eval(value);
+////	value = result;
+////	line.innerHTML = value;
+////});
+
+//delLine.addEventListener('click', () => {
+//	value = value.slice(0, -1);
+//	line.innerHTML = value;
+//});
+
+
+
 const one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
@@ -15,118 +170,131 @@ const division = document.getElementById('division');
 const delAll = document.getElementById('deleted-all');
 const delLine = document.getElementById('deleted-line');
 const result = document.getElementById('enter');
-let line = document.getElementById('line-text');
+const line = document.getElementById('line-text');
 let value = '';
+let prevOperator = '';
 let lastNum = '';
 
 zero.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += zero.value;
 	line.innerHTML = value;
-})
+});
 
 one.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += one.value;
 	line.innerHTML = value;
-})
+});
+
 two.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += two.value;
 	line.innerHTML = value;
-})
+});
+
 three.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += three.value;
 	line.innerHTML = value;
-})
+});
 
 four.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += four.value;
 	line.innerHTML = value;
-})
+});
 
 five.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += five.value;
 	line.innerHTML = value;
-})
+});
 
 six.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += six.value;
 	line.innerHTML = value;
-})
+});
 
 seven.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += seven.value;
 	line.innerHTML = value;
-})
+});
 
 eight.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += eight.value;
 	line.innerHTML = value;
-})
+});
 
 nine.addEventListener('click', () => {
-	value !== '' ? value += '' : console.log('');
 	value += nine.value;
 	line.innerHTML = value;
-})
+});
 
 plus.addEventListener('click', () => {
-	if (value !== '' && /[+\-*]$/.test(value)) {
-		// если последний символ - это уже оператор, заменяем его
-		value = value.slice(0, -1) + '+';
-	} else {
-		// иначе добавляем новый оператор
-		value += '+';
+	if (value === '') {
+		value = '0';
 	}
-	line.innerHTML = value;
-});
-
-multiply.addEventListener('click', () => {
-	if (value !== '' && /[+\-*]$/.test(value)) {
-		value = value.slice(0, -1) + '*';
-	} else {
-		value += '*';
+	else if (/[+\-*\/]$/.test(value)) {
+		value = value.slice(0, -1);
 	}
-	line.innerHTML = value;
-});
-
-division.addEventListener('click', () => {
-	if (value !== '' && /[+\-*]$/.test(value)) {
-		value = value.slice(0, -1) + '/';
-	} else {
-		value += '/';
-	}
+	value += '+';
+	prevOperator = '+';
 	line.innerHTML = value;
 });
 
 minus.addEventListener('click', () => {
-	if (value !== '' && /[+\-*]$/.test(value)) {
-		value = value.slice(0, -1) + '-';
-	} else {
-		value += '-';
+	if (value === '') {
+		value = '0';
 	}
+	else if (/[+\-*\/]$/.test(value)) {
+		value = value.slice(0, -1);
+	}
+	value += '-';
+	prevOperator = '-';
 	line.innerHTML = value;
 });
 
-delAll.addEventListener('click', () => {
-	line.innerHTML = '0';
-	value = '';
+multiply.addEventListener('click', () => {
+	if (value === '') {
+		value = '0';
+	}
+	else if (/[+\-*\/]$/.test(value)) {
+		value = value.slice(0, -1);
+	}
+	value += '*';
+	prevOperator = '*';
+	line.innerHTML = value;
+});
+
+division.addEventListener('click', () => {
+	if (value === '') {
+		value = '0';
+	}
+	else if (/[+\-*\/]$/.test(value)) {
+		value = value.slice(0, -1);
+	}
+	value += '/';
+	prevOperator = '/';
+	line.innerHTML = value;
 });
 
 result.addEventListener('click', () => {
-	const result = eval(value);
-	value = result;
-	line.innerHTML = value;
+	if (value === '') {
+		line.innerHTML = '0';
+	} else {
+		if (/[+\-*\/]$/.test(value)) {
+			value = value.slice(0, -1);
+		}
+		const result = eval(value);
+		line.innerHTML = result.toString();
+		value = result.toString();
+		prevOperator = '';
+	}
+});
+
+delAll.addEventListener('click', () => {
+	value = '';
+	prevOperator = '';
+	line.innerHTML = '0';
 });
 
 delLine.addEventListener('click', () => {
 	value = value.slice(0, -1);
 	line.innerHTML = value;
 });
+
